ef8969814d8bf738cf325e9dba8fec539f1de392
Arslan Qazi <arslanqazi1999@gmail.com>
2025-01-14 13:17:15 +0500

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,6 +10,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   TextEditingController nameController=TextEditingController();
+  TextEditingController emailController=TextEditingController();
   final FirebaseFirestore firestore=FirebaseFirestore.instance;
   bool loading=false;
   @override
@@ -31,6 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          //==================================================>>>>1st Text field
         TextFormField(
           controller: nameController,
           decoration: InputDecoration(
@@ -47,45 +48,76 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ),
-          ElevatedButton(
-              onPressed: () async{
-                try{
-                  loading=true;
+
+        SizedBox(height: 10,),
+          //==================================================>>>>2nd Text field
+          TextFormField(
+          controller: emailController,
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Enter your Email',
+            labelText: 'Enter your Email',
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Colors.grey,width: 2)
+            ),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide(color: Colors.grey,width: 2)
+            ),
+          ),
+        ),
+
+          SizedBox(height: 20,),
+          //==================================================>>>>ADD Button
+          loading?CircularProgressIndicator()
+              :ElevatedButton(
+            onPressed: () async{
+              try{
+                loading=true;
+                setState(() {
+
+                });
+                if(nameController.text.isNotEmpty){
+                  await firestore.collection('Data').add({
+                    'Email':emailController.text,
+                    'Name':nameController.text,
+                  });
+                  loading=false;
+                  setState(() {
+                  });
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text('Data Addes Succcessfuly')));
+                }
+                else{
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Please Enter Your Data'),
+                        action: SnackBarAction(label: 'Cancel', onPressed: (){}),
+                      ));
+                  loading=false;
                   setState(() {
 
                   });
-                  if(nameController.text.isNotEmpty){
-                    await firestore.collection('Data').add({
-                      'Data':nameController.text,
-                    });
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text('Data Addes Succcessfuly')));
-                  }
-                  else{
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Please Enter Your Data'),
-                          action: SnackBarAction(label: 'Cancel', onPressed: (){}),
-                        ));
-                  }
-                }catch (e){
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Error$e'),
-                        action: SnackBarAction(label: 'Cancel', onPressed: (){}),
-                      ));
                 }
+              }catch (e){
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Error$e'),
+                      action: SnackBarAction(label: 'Cancel', onPressed: (){}),
+                    ));
+              }
 
-              },
+            },
             style:  ElevatedButton.styleFrom(
               backgroundColor: Colors.green,
               minimumSize: Size(300, 50),
             ),
-              child: Text(
-            'ADD',
-            style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-                fontWeight: FontWeight.bold
+            child: Text(
+              'ADD',
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+              ),
             ),
-          ),
 
           ),
       ],),
