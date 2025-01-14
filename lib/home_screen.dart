5d6cb3cb26c9fd86259d90c74e641de377ad8df5
Arslan Qazi <arslanqazi1999@gmail.com>
2025-01-14 11:40:25 +0500

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,7 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   TextEditingController nameController=TextEditingController();
-
+  final FirebaseFirestore firestore=FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,7 +47,28 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
           ElevatedButton(
-              onPressed: (){},
+              onPressed: () async{
+                try{
+                  if(nameController.text.isNotEmpty){
+                    await firestore.collection('Data').add({
+                      'Data':nameController.text,
+                    });
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text('Data Addes Succcessfuly')));
+                  }
+                  else{
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Please Enter Your Data'),
+                          action: SnackBarAction(label: 'Cancel', onPressed: (){}),
+                        ));
+                  }
+                }catch (e){
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Error$e'),
+                        action: SnackBarAction(label: 'Cancel', onPressed: (){}),
+                      ));
+                }
+
+              },
             style:  ElevatedButton.styleFrom(
               backgroundColor: Colors.green,
               minimumSize: Size(300, 50),
