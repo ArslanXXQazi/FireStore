f21965e1ee94325ce55de1d939d945eb37d6aa38
Arslan Qazi <arslanqazi1999@gmail.com>
2025-01-18 20:24:53 +0500

    update

--- lib/fetch_data.dart
+++ lib/fetch_data.dart
@@ -1,6 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FetchData extends StatefulWidget {
@@ -14,23 +11,7 @@ class _FetchDataState extends State<FetchData> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Fetch Data',
-          style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
-        ),
-      ),
-      body: FutureBuilder(
-    future:FirebaseFirestore.instance.collection('Information').get() ,
-    builder: (context,AsyncSnapshot<QuerySnapshot>snapshot){
-      if (snapshot.connectionState==ConnectionState.waiting)
-        {
-          return Center(child: CircularProgressIndicator(),);
-        }
       
-    })
-
-          });
+    );
   }
 }
